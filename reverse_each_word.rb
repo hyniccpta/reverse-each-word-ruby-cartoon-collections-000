063f6d42faa6590ee13c77809f149d32eaaf772e
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-03-04 16:06:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,8 @@ reverse_each_word("Hello there, and how are you?")
 
 
 def reverse_each_word(greeting)  # using .collect method 
-  greeting.split.collect {|word| word.reverse}
+  array = greeting.split
+  array.collect {|word| word.reverse}
   greeting.join(" ")
   greeting.rstrip
   
