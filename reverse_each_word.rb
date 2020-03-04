56f7128bce12e68a688200fbc49624245cad5079
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-03-04 15:48:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,14 @@
-def reverse_each_word(greeting)
+def reverse_each_word(greeting) # using .each method
   array = greeting.split(" ")
   new_array = []
   array.each {|word| new_array << word.reverse}
   new_array.join(" ")
 end
-
 reverse_each_word("Hello there, and how are you?")
 
-def reverse_each_word(greeting)
-  array = greeting.split(" ")
-  new_array = []
-  array.collect {|word| new_array << word.reverse}
-  new_array.join(" ")
+
+def reverse_each_word(greeting)  # using .collect method 
+  greeting.split.collect {|word| word.reverse + " "}
 end
 reverse_each_word("Hello there, and how are you?")
+
